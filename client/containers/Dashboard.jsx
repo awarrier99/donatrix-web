5993bcf5a8b07134466530a0aeac8993aa37f0ca
Ashvin Warrier <warrierar@gmail.com>
2018-11-29 00:58:40 -0500

    Created skeleton dashboard

--- client/containers/Dashboard.jsx
+++ client/containers/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Card, Row } from 'antd';
 import { getUser } from '../util';
 import styles from './Dashboard.css';
@@ -24,7 +25,7 @@ class Dashboard extends Component {
         <div style={{ display: 'table-cell', verticalAlign: 'middle' }}>
           <Row type="flex" justify="center">
             <Card className={styles.card} title="Items">
-              <p>{this.userType === 'LOCATION_EMPLOYEE' ? 'Add, edit, and view items' : 'View items'}</p>
+              <Link to="/items">{this.userType === 'LOCATION_EMPLOYEE' ? 'Add, edit, and view items' : 'View items'}</Link>
             </Card>
             <Card className={styles.card} title="Items">
               <p>Add, edit, and view items</p>
