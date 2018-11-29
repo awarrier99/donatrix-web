c491960e5ad676b4ca36a3c213045c9db9e04b12
Ashvin Warrier <warrierar@gmail.com>
2018-11-29 00:54:24 -0500

    Removed debug statement

---
+++ client/containers/ItemsDashboard.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class ItemsDashboard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  render() {
+    return (
+      <p>Hello</p>
+    );
+  }
+}
+
+export default ItemsDashboard;
